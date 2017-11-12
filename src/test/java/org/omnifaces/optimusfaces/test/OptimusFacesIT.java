612427fd783e1a7e4a0f935a605ae26c6b22dfae
Bauke Scholtz <balusc@gmail.com>
2017-11-12 02:18:50 +0100

    Add Payara 5 with Hibernate 5.2 (JPA 2.2) to IT

--- src/test/java/org/omnifaces/optimusfaces/test/OptimusFacesIT.java
+++ src/test/java/org/omnifaces/optimusfaces/test/OptimusFacesIT.java
@@ -105,6 +105,14 @@ public abstract class OptimusFacesIT {
 		if (isWildFly()) {
 			archive.addAsWebInfResource("WEB-INF/wildfly-ds.xml/" + database.name().toLowerCase() + ".xml", "wildfly-ds.xml");
 		}
+		else if (isPayara()) {
+			archive.addAsWebInfResource("WEB-INF/glassfish-resources.xml/" + database.name().toLowerCase() + ".xml", "glassfish-resources.xml");
+
+			if (isHibernate()) {
+				// Does not work when placed in glassfish/modules? TODO: investigate
+				archive.addAsLibraries(maven.resolve("org.hibernate:hibernate-core:5.2.12.Final", "dom4j:dom4j:1.6.1").withTransitivity().asFile());
+			}
+		}
 		else if (isTomEE()) {
 			archive.addAsWebInfResource("WEB-INF/resources.xml/" + database.name().toLowerCase() + ".xml", "resources.xml");
 		}
@@ -220,6 +228,10 @@ public abstract class OptimusFacesIT {
 		return System.getProperty("profile.id").startsWith("wildfly-");
 	}
 
+	protected static boolean isPayara() {
+		return System.getProperty("profile.id").startsWith("payara-");
+	}
+
 	protected static boolean isTomEE() {
 		return System.getProperty("profile.id").startsWith("tomee-");
 	}
@@ -242,6 +254,10 @@ public abstract class OptimusFacesIT {
 
 	protected boolean isLazy() {
 		return browser.getCurrentUrl().contains("OptimusFacesITLazy");
+	}
+
+	protected boolean isJPA22() {
+		return isPayara(); // For now, the Payara test is the only one which uses JPA 2.2 (with LocalDate support).
 	}
 
 
@@ -621,8 +637,8 @@ public abstract class OptimusFacesIT {
 		assertPaginatorState(1, 38);
 		assertFilteredState(idColumnFilter, "3");
 
-		if ((isOpenJPA() || isEclipseLink()) && isPostgreSQL()) {
-			System.out.println("SKIPPING globalFilter test for OpenJPA and EclipseLink on PostgreSQL because it doesn't support LocalDate in LIKE");
+		if (!isJPA22() && (isOpenJPA() || isEclipseLink()) && isPostgreSQL()) {
+			System.out.println("SKIPPING globalFilter test for non-JPA 2.2 OpenJPA and EclipseLink on PostgreSQL because it doesn't support LocalDate in LIKE");
 			// org.postgresql.util.PSQLException: ERROR: function lower(bytea) does not exist
 		}
 		else {
@@ -688,8 +704,8 @@ public abstract class OptimusFacesIT {
 		assertFilteredState(emailColumnFilter, "1");
 		assertSortedState(emailColumn, true);
 
-		if ((isOpenJPA() || isEclipseLink()) && isPostgreSQL()) {
-			System.out.println("SKIPPING globalFilter test for OpenJPA and EclipseLink on PostgreSQL because it doesn't support LocalDate in LIKE");
+		if (!isJPA22() && (isOpenJPA() || isEclipseLink()) && isPostgreSQL()) {
+			System.out.println("SKIPPING globalFilter test for non-JPA 2.2 OpenJPA and EclipseLink on PostgreSQL because it doesn't support LocalDate in LIKE");
 			// org.postgresql.util.PSQLException: ERROR: function lower(bytea) does not exist
 		}
 		else {
@@ -734,8 +750,8 @@ public abstract class OptimusFacesIT {
 		assertPaginatorState(1, 38);
 		assertSortedState(emailColumn, true);
 
-		if ((isOpenJPA() || isEclipseLink()) && isPostgreSQL()) {
-			System.out.println("SKIPPING globalFilter test for OpenJPA and EclipseLink on PostgreSQL because it doesn't support LocalDate in LIKE");
+		if (!isJPA22() && (isOpenJPA() || isEclipseLink()) && isPostgreSQL()) {
+			System.out.println("SKIPPING globalFilter test for non-JPA 2.2 OpenJPA and EclipseLink on PostgreSQL because it doesn't support LocalDate in LIKE");
 			// org.postgresql.util.PSQLException: ERROR: function lower(bytea) does not exist
 		}
 		else {
